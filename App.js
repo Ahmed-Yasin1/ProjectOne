96c176929084e07b6691d3e2bc4e2ffc62ab1867
Ahmed-Yasin <xasancumar063@gmail.com>
2024-03-11 00:47:16 -0700

    add style

--- App.js
+++ App.js
@@ -8,15 +8,19 @@
     let paragh = document.getElementById("p");
     let options = document.querySelectorAll("#option");
     let list= document.querySelector(".hide");
+    let rotate=document.querySelector(".rotate");
    
     select.onclick= function(){
         list.classList.toggle("hide");
+        rotate.classList.toggle("rotate");
     }
 
 
     for(option of options){
         option.onclick=function(){
             paragh.innerHTML= this.querySelector("p").textContent;
+            list.classList.toggle("hide");
+            rotate.classList.toggle("rotate");
         }
     }
 
